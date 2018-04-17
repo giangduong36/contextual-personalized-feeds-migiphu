2f1770289babb3fb654fbe540823cf6c5aa6b8c1
giangduong36 <gduong@macalester.edu>
2018-04-17 11:06:05 -0500

    Update view for comments of a post and posts of a page

--- recsys/views.py
+++ recsys/views.py
@@ -6,7 +6,8 @@ from django.forms import ModelForm
 
 # Create your views here.
 
-from .models import Post, Page
+from .models import Post, Page, Comment
+
 
 def post_list(request):
     latest_post_list = Post.objects.order_by('created_time')[:50]
@@ -22,21 +23,28 @@ def post_detail(request, post_id):
     try:
         post = Post.objects.get(id=post_id)
         form = PostForm(instance=post)
+        comments = Comment.objects.filter(post_id = post)
     except Post.DoesNotExist:
         raise Http404("Post does not exist")
 
-    return render(request, 'recsys/post_detail.html', {'form': form})
+    return render(request, 'recsys/post_detail.html', {'form': form, 'comments': comments})
 
 
 def page_list(request):
     latest_page_list = Page.objects.all()
-    context = {'latest_page_list':latest_page_list}
+    context = {'latest_page_list': latest_page_list}
     return render(request, 'recsys/page_list.html', context)
 
 
 def page_detail(request, page_id):
-    page = get_object_or_404(Page, pk=page_id)
-    return render(request, 'recsys/page_detail.html', {'page': page})
+    try:
+        page = Page.objects.get(id=page_id)
+        posts_of_page = Post.objects.filter(page_id=page)   # All posts that belong to this page
+        context = {'latest_post_list': posts_of_page, 'page_name': page.name}
+    except posts_of_page.DoesNotExist:
+        raise Http404("Post does not exist")
+
+    return render(request, 'recsys/page_detail.html', context)
 
 
 def comment_list(request):
