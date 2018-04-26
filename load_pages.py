f63d82e9b7d66efcaeb8e7c118051aa3084c384f
phuchng <hongphucng63@gmail.com>
2018-04-26 10:53:46 -0500

    deleted aggregate_concat.py

---
+++ load_pages.py
@@ -0,0 +1,46 @@
+import sys, os
+import pandas as pd
+import datetime
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recsys440.settings")
+
+import django
+
+django.setup()
+
+from recsys.models import Page
+
+
+# la_times and los_angeles_times are duplicated with the same id --> Only 82 pages
+
+def save_page_from_row(page_row):
+    page = Page()
+    page.name = page_row[0]
+    page.id = page_row[1]
+    page.save()
+
+
+def delete_db():
+    print('truncate db')
+    Page.objects.all().delete()
+    print('finished truncate db')
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 2:
+        delete_db()
+        print("Reading from file " + str(sys.argv[1]))
+        pages_df = pd.read_csv(sys.argv[1])
+        print(pages_df.head())
+        print(pages_df.info())
+
+        pages_df.apply(
+            save_page_from_row,
+            axis=1
+        )
+
+        print("There are {} pages in DB".format(Page.objects.count()))
+
+    else:
+        print("Please, provide Pages file path")
